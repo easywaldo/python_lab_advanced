399d964ef27f2d12c08570dd3fcb1347a22c9eab
easywaldo <easywaldoji@gmail.com>
2022-06-06 23:40:59 +0900

    다수의 메시지를 큐에 적재하여 컨슈밍 할 수 있도록 변경

--- ch-message-queue/sender.py
+++ ch-message-queue/sender.py
@@ -6,7 +6,9 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='hello world')
-print('# 메시지를 보냈습니다!!!')
+for i in range(10000):
+    # 10,000 개의 메시지를 큐에 적재!!
+    channel.basic_publish(exchange='', routing_key='hello', body=f'this is message number {i}')
+    print('# 메시지를 보냈습니다!!! %s' % str(i))
 
 connection.close()
