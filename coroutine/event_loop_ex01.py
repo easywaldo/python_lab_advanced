f51d68658cf1da214cceb840e049333c53031d83
jinam.lee <jinam.lee@a-bly.com>
2022-06-18 12:39:18 +0900

    언제나 같은 이벤트루프 얻기

--- coroutine/event_loop_ex01.py
+++ coroutine/event_loop_ex01.py
@@ -7,3 +7,14 @@ async def func():
 loop = asyncio.get_event_loop()
 coro = func()
 print(loop.run_until_complete(coro))
+
+
+# 이벤트루프를 얻는 2가지 방법
+"""
+추천 : asyncio.get_running_loop() >> 코루틴 내에서 호출 가능
+비추천 : asyncio.get_event_loop() >> 어디서든 호출 가능
+"""
+
+loop1 = asyncio.get_event_loop()
+loop2 = asyncio.get_event_loop()
+print(loop1 is loop2)
