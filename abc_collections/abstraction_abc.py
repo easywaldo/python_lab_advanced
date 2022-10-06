d09927c060fc54d4fd9dd826c70a0304f51d0277
easywaldo <easywaldoji@gmail.com>
2022-10-07 00:15:25 +0900

    abstraction using abc.collections

---
+++ abc_collections/abstraction_abc.py
@@ -0,0 +1,34 @@
+import collections
+class AliasedIngredients(collections.abc.Set):
+    def __init__(self, ingredients: set[str]):
+        self.ingredients = ingredients
+        
+    def __contains__(self, value: str):
+        return value in self.ingredients or \
+            any(alias in self.ingredients for alias in get_aliases(value))
+    
+    def __iter__(self):
+        return iter(self.ingredients)
+    
+    def __len__(self):
+        return len(self.ingredients)
+
+
+def get_aliases(alias: str) -> list[str]:
+    aliases = {
+        "rocket": "arugula",
+        "jumbo": "orange",    
+    }
+
+
+if __name__ == "__main__":
+    ingredients = AliasedIngredients({"arugula", "eggplant", "pepper"})
+
+    for ingredients in ingredients:
+        print(ingredients)
+
+    print(len(ingredients))
+
+    print('arugula' in ingredients)
+    print('rocket' in ingredients)
+    print(list(ingredients | AliasedIngredients({'garlic'})))
