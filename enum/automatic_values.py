59e0e03d847ea872a2447dc5cdcad525b287b76b
easywaldo <easywaldoji@gmail.com>
2022-10-10 23:45:31 +0900

    generate_next_value

--- enum/automatic_values.py
+++ enum/automatic_values.py
@@ -1,5 +1,8 @@
 from enum import auto, Enum
 class MotherSauce(Enum):
+    def _generate_next_value_(name, start, count, last_values):
+        return name.capitalize()
+    
     BÉCHAMEL = auto()
     VELOUTÉ = auto()
     ESPAGNOLE = auto()
