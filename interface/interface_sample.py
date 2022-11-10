624c03bf3937c38e06c264ee6d53e4d2731b165f
easywaldo <easywaldoji@gmail.com>
2022-11-10 23:52:41 +0900

    interface initializing sample

--- interface/interface_sample.py
+++ interface/interface_sample.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import decimal
 from enum import auto, Enum
 
 class ImperialMeasure(Enum):
@@ -28,3 +29,22 @@ test_ingredients = Ingredient(
 )
 
 print(test_ingredients)
+
+
+@dataclass(frozen=True)
+class Item:
+    name: str
+    brand: str
+    measure: ImperialMeasure
+    price_in_cents: decimal.Decimal
+    amount: float
+    
+test_item = Item(
+    name="test",
+    brand="test_brand",
+    measure=ImperialMeasure.CUP,
+    price_in_cents=10.5,
+    amount=9.8
+)
+
+print(test_item)
