778251420c2dfa2c2ff0813bdf7090d69f1e26f2
easywaldo <easywaldoji@gmail.com>
2022-11-27 19:48:29 +0900

    async await event loop sample

--- asyncio/hello_world_2.py
+++ asyncio/hello_world_2.py
@@ -1,17 +1,34 @@
 import asyncio
 
-async def hello_world():
-    print("hello world")
-    return 42
+# async def hello_world():
+#     print("hello world")
+#     return 42
 
-hello_world_coroutine = hello_world()
+# hello_world_coroutine = hello_world()
 
-print(hello_world_coroutine)
+# print(hello_world_coroutine)
 
-event_loop = asyncio.get_event_loop()
+# event_loop = asyncio.get_event_loop()
+# try:
+#     print("entering event loop")
+#     result = event_loop.run_until_complete(hello_world_coroutine)
+#     print(result)
+# finally:
+#     event_loop.close()
+    
+    
+async def add_42(number):
+    print("Adding 42")
+    return 42 + number
+
+async def hello_world_s():
+    print("hello world async def")
+    result = await add_42(23)
+    return result
+
+event_loop_s = asyncio.get_event_loop()
 try:
-    print("entering event loop")
-    result = event_loop.run_until_complete(hello_world_coroutine)
+    result = event_loop_s.run_until_complete(hello_world_s())
     print(result)
 finally:
-    event_loop.close()
+    event_loop_s.close()
