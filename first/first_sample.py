5d84e41ddd05115ad901607b5a0a05b01b58195b
easywaldo <easywaldoji@gmail.com>
2022-11-29 23:50:51 +0900

    first second sample added

--- first/first_sample.py
+++ first/first_sample.py
@@ -7,3 +7,8 @@ print(first([-1, 0, 1, 2]))
 print(first([-1, 0, 1, 2, 3, 4, 5], key=lambda x: x > 0))
 
 
+import operator
+def greater_than_five(number):
+    return number > 5
+
+print(first([-1, 0, 2, 4, 5, 10, 18], key=greater_than_five))
