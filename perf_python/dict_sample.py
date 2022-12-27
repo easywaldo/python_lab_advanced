efd948177dc63fc2c3686a8a01f5c8ac0c0016a1
jinam.lee <jinam.lee@a-bly.com>
2022-12-28 00:10:10 +0900

    search unique name in list, set

--- perf_python/dict_sample.py
+++ perf_python/dict_sample.py
@@ -18,13 +18,30 @@ phonebook_dict = {
 
 print(f"Easywaldo phone number is {phonebook_dict['Easywaldo']}")
 
-def list_unique_names(phonbook):
+def list_unique_names(phonebook):
     unique_names = []
     for name, phonenumber in phonebook:
         first_name, last_name = name.split(" ", 1)
         for unique in unique_names:
             if unique == first_name:
                 break
-            else:
-                unique_names.append(first_name)
-        return len(unique_names)
+        else:
+            unique_names.append(first_name)
+    return len(unique_names)
+    
+def set_unique_names(phonebook):
+    unique_names = set()
+    for name, phonenumber in phonebook:
+        first_name, last_name = name.split(" ", 1)
+        unique_names.add(first_name)
+    return len(unique_names)
+
+phonebook = [
+    ("John Doe", "999-999-9999"),
+    ("Albert Einstein", "1234-211-1214"),
+    ("Albert Rutherford", "8112-923-1924"),
+    ("Guido van Rossum", "9201-234-1290"),
+]
+
+print("Number of unique names form set method:", set_unique_names(phonebook))
+print("Number of unique names from list method: ", list_unique_names(phonebook))
