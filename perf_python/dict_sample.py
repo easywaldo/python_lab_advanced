5a7f8d901c6a6be23caf9e06affa97252622f5b4
jinam.lee <jinam.lee@a-bly.com>
2022-12-27 23:53:39 +0900

    searching list  value type

---
+++ perf_python/dict_sample.py
@@ -0,0 +1,12 @@
+def find_phonenumber(phonebook, name):
+    for n, p in phonebook:
+        if n == name:
+            return p
+    return None
+
+phonebook = [
+    ("John Doe", "123-456-7890"),
+    ("Easywaldo", "999-999-9999"),
+]
+
+print(f"Easywaldo phone number is {find_phonenumber(phonebook, 'Easywaldo')}")
