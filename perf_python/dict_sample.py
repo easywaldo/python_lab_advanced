5766bdf72cd5286bce54b65e4b7b8e5797b703ab
jinam.lee <jinam.lee@a-bly.com>
2022-12-27 23:58:56 +0900

    search unique name in list

--- perf_python/dict_sample.py
+++ perf_python/dict_sample.py
@@ -17,3 +17,14 @@ phonebook_dict = {
 }
 
 print(f"Easywaldo phone number is {phonebook_dict['Easywaldo']}")
+
+def list_unique_names(phonbook):
+    unique_names = []
+    for name, phonenumber in phonebook:
+        first_name, last_name = name.split(" ", 1)
+        for unique in unique_names:
+            if unique == first_name:
+                break
+            else:
+                unique_names.append(first_name)
+        return len(unique_names)
