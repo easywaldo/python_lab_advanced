15beafb6fe7366403483fcf3b00ba83f353e9026
jinam.lee <jinam.lee@a-bly.com>
2023-01-06 23:56:56 +0900

    sorted function

--- fluent_python/ch07/function_as_first_class_object.py
+++ fluent_python/ch07/function_as_first_class_object.py
@@ -4,3 +4,9 @@ from operator import add
 
 print(reduce(add, range(100)))
 print(sum(range(100)))
+
+
+fruits = ['fig', 'strawberry', 'apple', 'cherry', 'raspberry', 'banana']
+result = sorted(fruits, key=lambda word: word[::-1])
+print(result)
+print(fruits)
