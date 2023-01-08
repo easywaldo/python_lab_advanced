93bf61ea205c99a794242279959c141348869abd
easywaldo <easywaldoji@gmail.com>
2023-01-08 23:25:53 +0900

    ChainMap sample

--- fluent_python/ch03/dict_pattern_matching.py
+++ fluent_python/ch03/dict_pattern_matching.py
@@ -51,3 +51,15 @@ h1 = (1, 2, [30, 40])
 
 tuple_frozen = (1, 2, frozenset([30, 40]))
 print(hash(tuple_frozen))
+
+
+d1 = dict(a=1, b=2)
+d2 = dict(a=2, b=4, c=6)
+from collections import ChainMap
+
+chain = ChainMap(d1, d2)
+print(chain['a'])
+print(chain['c'])
+chain['c']=-1
+print(d1)
+print(d2)
