eed28219d4c8e56da298e230f596bb912ad20b26
easywaldo <easywaldoji@gmail.com>
2023-01-08 23:32:13 +0900

    Immutable Mappings

--- fluent_python/ch03/dict_pattern_matching.py
+++ fluent_python/ch03/dict_pattern_matching.py
@@ -72,3 +72,14 @@ s = {'a', 'e', 'i'}
 print(d1.keys() & s)
 
 print(d1.keys() | s)
+
+
+from types import MappingProxyType
+d = {1: 'A'}
+d_proxy = MappingProxyType(d)
+print(d_proxy)
+
+# d_proxy[1] = 'x' # TypeError: 'mappingproxy' object does not support item assignment
+d[2] = 'B'
+print(d_proxy)
+print(d_proxy[2])
