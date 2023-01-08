8f18b6ae83f70eb1435f87b0c432f8c6d46a1b8d
easywaldo <easywaldoji@gmail.com>
2023-01-08 23:28:19 +0900

    and, or operation on dict

--- fluent_python/ch03/dict_pattern_matching.py
+++ fluent_python/ch03/dict_pattern_matching.py
@@ -63,3 +63,12 @@ print(chain['c'])
 chain['c']=-1
 print(d1)
 print(d2)
+
+d1 = dict(a=1, b=2, c=3, d=4)
+d2 = dict(b=20, d=40, e=50)
+print(d1.keys() & d2.keys())
+
+s = {'a', 'e', 'i'}
+print(d1.keys() & s)
+
+print(d1.keys() | s)
