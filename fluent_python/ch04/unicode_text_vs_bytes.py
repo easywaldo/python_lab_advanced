d3944e30a338243040c330df3f961c1a49ffc0ad
easywaldo <easywaldoji@gmail.com>
2023-01-10 23:47:32 +0900

    a five-byte sequence as bytes and as bytearray

--- fluent_python/ch04/unicode_text_vs_bytes.py
+++ fluent_python/ch04/unicode_text_vs_bytes.py
@@ -7,3 +7,15 @@ print(b)
 print(len(b))
 
 print(b.decode('utf8'))
+
+
+cafe = bytes('café', encoding='utf_8')
+print(cafe)
+
+print(cafe[0])
+print(cafe[:1])
+
+cafe_arr = bytearray(cafe)
+print(cafe_arr)
+
+print(cafe_arr[-1:])
