e318ea9b2cc778578813e1318bed97e72e255baf
easywaldo <easywaldoji@gmail.com>
2023-01-11 00:03:20 +0900

    three codecs produce different byte sequences

--- fluent_python/ch04/unicode_text_vs_bytes.py
+++ fluent_python/ch04/unicode_text_vs_bytes.py
@@ -26,3 +26,7 @@ import array
 numbers = array.array('h', [-2, -1, 0, 1, 2])
 octets = bytes(numbers)
 print(octets)
+
+
+for codec in ['latin_1', 'utf_8', 'utf_16']:
+    print(codec, 'El Niño'.encode(codec), sep='\t')
