f8deec8cbb497a6311bc10be361ec1d294867556
easywaldo <easywaldoji@gmail.com>
2023-01-10 23:57:10 +0900

    initializing bytes from the raw data of an array

--- fluent_python/ch04/unicode_text_vs_bytes.py
+++ fluent_python/ch04/unicode_text_vs_bytes.py
@@ -19,3 +19,10 @@ cafe_arr = bytearray(cafe)
 print(cafe_arr)
 
 print(cafe_arr[-1:])
+
+
+print(bytes.fromhex('31 4B CE A9'))
+import array
+numbers = array.array('h', [-2, -1, 0, 1, 2])
+octets = bytes(numbers)
+print(octets)
