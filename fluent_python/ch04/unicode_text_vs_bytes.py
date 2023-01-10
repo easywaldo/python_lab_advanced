d5e3479d1e2d19624a6e7ab7473afe3fbd10267b
easywaldo <easywaldoji@gmail.com>
2023-01-11 00:21:28 +0900

    Decoding from str to bytes: success and error handling

--- fluent_python/ch04/unicode_text_vs_bytes.py
+++ fluent_python/ch04/unicode_text_vs_bytes.py
@@ -44,4 +44,10 @@ print(city.encode('cp437', errors='replace'))
 print(city.encode('cp437', errors='xmlcharrefreplace'))
 
 
+octets = b'Montr\xe9al'
+print(octets.decode('cp1252'))
+print(octets.decode('iso8859_7'))
+print(octets.decode('koi8_r'))
+# print(octets.decode('utf_8')) # error
 
+print(octets.decode('utf_8', errors='replace'))
