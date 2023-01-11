980608de2536787a6826449792872b4e0147e1e6
easywaldo <easywaldoji@gmail.com>
2023-01-12 01:07:51 +0900

    NamedTuple sub type class

--- fluent_python/ch05/data_class.py
+++ fluent_python/ch05/data_class.py
@@ -28,3 +28,17 @@ print(typing.get_type_hints(Coordinate))
 Coordinate = typing.NamedTuple('Coordinate',lat=float,lon=float)
 print(issubclass(Coordinate, tuple))
 print(typing.get_type_hints(Coordinate))
+
+from typing import NamedTuple
+
+class Coordinate(NamedTuple):
+    lat: float
+    lon: float
+    
+    def __str__(self):
+        ns = 'N' if self.lat >= 0 else 'S'
+        we = 'E' if self.lon >= 0 else 'W'
+        return f'{abs(self.lat):.1f}°{ns}, {abs(self.lon):.1f}°{we}'
+    
+# print(issubclass(Coordinate, typing.NamedTuple)) # error
+print(issubclass(Coordinate, tuple))
