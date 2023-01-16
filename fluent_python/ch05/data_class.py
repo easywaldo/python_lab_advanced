b727c0311793a105edb21491d9929a647aaa29a3
easywaldo <easywaldoji@gmail.com>
2023-01-16 23:56:25 +0900

    __match_args__

--- fluent_python/ch05/data_class.py
+++ fluent_python/ch05/data_class.py
@@ -185,3 +185,44 @@ book = Resource(
     ['computer programming', 'OOP'])
 
 print(book)
+
+
+class City(typing.NamedTuple):
+    continent: str
+    name: str
+    country: str
+    
+
+cities = [
+    City('Asia', 'Tokyo', 'JP'),
+    City('Asia', 'Delhi', 'IN'),
+    City('North America', 'Mexico City', 'MX'),
+    City('North America', 'New York', 'US'),
+    City('South America', 'São Paulo', 'BR'),
+]
+    
+def match_asian_cities():
+    results = []
+    for city in cities:
+        match city:
+            case City(continent='Asia'):
+                results.append(city)
+    return results
+
+def match_asian_countries():
+    results = []
+    for city in cities:
+        match city:
+            case City(continent='Asia', country=cc):
+                results.append(cc)
+    return results
+
+def match_asian_cities_pos():
+    results = []
+    for city in cities:
+        match city:
+            case City('Asia'):
+                results.append(city)
+    return results
+
+print(City.__match_args__)
