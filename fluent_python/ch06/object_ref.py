244a323087183255dd0dabce1173867a78b9cd24
easywaldo <easywaldoji@gmail.com>
2023-01-18 23:58:03 +0900

    refer to the same object

---
+++ fluent_python/ch06/object_ref.py
@@ -0,0 +1,9 @@
+charles = {'name': 'Charles L. Doggson', 'born': 1832}
+lewis = charles
+print(lewis is charles)
+
+print(id(charles), id(lewis))
+
+lewis['balance'] = 950
+print(charles)
+print(lewis)
