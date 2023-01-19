e7f0c8d2cf461f0184cdce87ad3aa79af67a4d25
easywaldo <easywaldoji@gmail.com>
2023-01-19 23:23:15 +0900

    changing mutable item inside tuple

--- fluent_python/ch06/object_ref.py
+++ fluent_python/ch06/object_ref.py
@@ -12,3 +12,15 @@ alex = {'name': 'Charles L. Doggson', 'born': 1832, 'balance': 950}
 print(alex == charles)
 
 print(alex is not charles)
+
+t1 = (1, 2, [30, 40])
+t2 = (1, 2, [30, 40])
+print(t1 == t2)
+
+print(id(t1[-1]))
+
+t1[-1].append(99)
+print(t1)
+
+print(id(t1[-1]))
+print(t1 == t2)
