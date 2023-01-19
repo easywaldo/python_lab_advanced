67a2a34999a222a061122fb52f6dba4672ec0891
easywaldo <easywaldoji@gmail.com>
2023-01-19 23:49:50 +0900

    Effects of using copy vs deepcopy

--- fluent_python/ch06/object_ref.py
+++ fluent_python/ch06/object_ref.py
@@ -49,3 +49,29 @@ l2[2] += (10, 11)   # 세번째 요소에 += 튜플연산자 로 인한 새로�
 
 print('l1:', l1)
 print('l2:', l2)
+
+
+class Bus:
+    def __init__(self, passengers=None):
+        if passengers is None:
+            self.passengers = []
+        else:
+            self.passengers = list(passengers)
+        
+    def pick(self, name):
+        self.passengers.append(name)
+    
+    def drop(self, name):
+        self.passengers.remove(name)
+        
+import copy
+bus1 = Bus(['Alice', 'Bill', 'Claire', 'David'])
+bus2 = copy.copy(bus1)
+bus3 = copy.deepcopy(bus1)
+print(id(bus1), id(bus2), id(bus3))
+
+bus1.drop('Bill')
+print(bus2.passengers)
+
+print(id(bus1.passengers), id(bus2.passengers), id(bus3.passengers))
+print(bus3.passengers)
