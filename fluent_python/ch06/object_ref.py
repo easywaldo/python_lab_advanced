5d21c1cc373edd887a6962c91626f3a9813822d8
jinam.lee <jinam.lee@a-bly.com>
2023-01-20 23:51:46 +0900

    buses haunted by ghost passengers

--- fluent_python/ch06/object_ref.py
+++ fluent_python/ch06/object_ref.py
@@ -105,3 +105,46 @@ u = (30, 40)
 print(f(t, u))
 
 print(t, u)
+
+
+class HauntedBus:
+    """
+    Haunted Bus
+    """
+    
+    def __init__(self, passengers=[]):
+        self.passengers = passengers
+    
+    def pick(self, name):
+        self.passengers.append(name)
+        
+    def drop(self, name):
+        self.passengers.remove(name)
+        
+
+bus1 = HauntedBus(["Alice", "Bill"])
+print(bus1)
+
+bus1.pick('Charlie')     
+bus1.drop('Alice')
+
+print(bus1.passengers)
+   
+   
+bus2 = HauntedBus()
+bus2.pick('Carrie')
+print(bus2.passengers)
+
+
+bus3 = HauntedBus()
+print(bus3.passengers)
+
+
+bus3.pick('Dave')
+print(bus2.passengers)
+
+
+print(bus2.passengers is bus3.passengers)
+print(bus1.passengers)
+
+print(bus1.passengers is bus2.passengers)
