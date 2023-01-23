76a12f4b68e138092a465bd98d4345a19af883c8
jinam.lee <jinam.lee@a-bly.com>
2023-01-23 16:11:27 +0900

    sample attrgetter

--- fluent_python/ch07/function_as_first_class_objects_sample.py
+++ fluent_python/ch07/function_as_first_class_objects_sample.py
@@ -14,3 +14,19 @@ for city in sorted(metro_data, key=itemgetter(1)):
 cc_name = itemgetter(1, 0)
 for city in metro_data:
     print(cc_name(city))
+
+
+from collections import namedtuple
+LatLon = namedtuple('LatLon', 'lat lon')
+Metropolis = namedtuple('Metropolis', 'name cc pop coord')
+metro_areas = [Metropolis(name, cc, pop, LatLon(lat, lon))
+               for name, cc, pop, (lat, lon) in metro_data]
+
+print(metro_areas[0])
+
+print(metro_areas[0].coord.lat)
+
+from operator import attrgetter
+name_lat = attrgetter('name', 'coord.lat')
+for city in sorted(metro_areas, key=attrgetter('coord.lat')):
+    print(name_lat(city))
