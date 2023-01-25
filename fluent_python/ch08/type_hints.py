c856ff35ea91d9e73ec59e7123d45543a409ba0d
easywaldo <easywaldoji@gmail.com>
2023-01-25 23:59:48 +0900

    type hint

---
+++ fluent_python/ch08/type_hints.py
@@ -0,0 +1,19 @@
+from pytest import mark
+
+def show_count(count, word):
+    if count == 1:
+        return f'1 {word}'
+    count_str = str(count) if count else 'no'
+    return f'{count_str} {word}s'
+
+@mark.parametrize('qty, expected', [
+    (1, '1 part'),
+    (2, '2 parts'),
+])
+def test_show_count(qty, expected):
+    got = show_count(qty, 'part')
+    assert got == expected
+
+def test_show_count_zero():
+    got = show_count(0, 'part')
+    assert got == 'no parts'
