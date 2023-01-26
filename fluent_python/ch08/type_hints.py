0c92e51e789137a3b270b0a57ad258664f517da9
easywaldo <easywaldoji@gmail.com>
2023-01-27 01:22:55 +0900

    mypy check type of object

--- fluent_python/ch08/type_hints.py
+++ fluent_python/ch08/type_hints.py
@@ -37,3 +37,20 @@ def test_double_seq():
     seq_num = yield [n for n in range(1,5)]
     assert double(seq_num) == list(2,4,6,8,10)
     assert double("hello world") == "hello worldhello world"
+
+
+class Bird:
+    pass
+
+class Duck(Bird):
+    def quack(self):
+        print('Quack')
+
+def alert(birdie):
+    birdie.quack()
+    
+def alert_duck(birdie: Duck) -> None:
+    birdie.quack()
+    
+def alert_bird(birdie: Bird) -> None:
+    birdie.quack()  # error: "Bird" has no attribute "quack"
