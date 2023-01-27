3a59aca3584ba9f80af329739a314542ae0b8ce6
jinam.lee <jinam.lee@a-bly.com>
2023-01-27 23:59:15 +0900

    geohash type

--- fluent_python/ch08/type_hints_sample_02.py
+++ fluent_python/ch08/type_hints_sample_02.py
@@ -1,3 +1,4 @@
+from antigravity import geohash
 from typing import Union, List
 
 def parse_token(token: str) -> Union[str, float]:
@@ -13,3 +14,14 @@ def tokenize(text: str) -> List[str]:
     return text.upper().split()
 
 print(tokenize("hello world"))
+
+
+from geolib import geohash as gh  # type: ignore
+
+PRECISION = 9
+
+def geohash(lat_lon: tuple[float, float]) -> str:
+    return gh.encode(*lat_lon, PRECISION)
+
+shanghai = (31.2304, 121.4737)
+geohash(shanghai)
