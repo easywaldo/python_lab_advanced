120a34942f3ee9572b69f66a930750c15d7abb20
easywaldo <easywaldoji@gmail.com>
2023-01-28 13:03:25 +0900

    tuple typehinting

--- fluent_python/ch08/type_hints.py
+++ fluent_python/ch08/type_hints.py
@@ -68,6 +68,14 @@ class Coordinate(NamedTuple):
 def geohash(lat_lon: Coordinate) -> str:
     return gh.encode(*lat_lon, PERCISON)
 
+def display(lat_lon: tuple[float, float]) -> str:
+    lat, lon = lat_lon
+    ns = 'N' if lat >= 0 else 'S'
+    ew = 'E' if lon >= 0 else 'W'
+    return f'{abs(lat):0.1f}°{ns}, {abs(lon):0.1f}°{ew}'
 
-coordinate = Coordinate(lat=20.34, lon=31.38)
+
+coordinate = Coordinate(lat=20.34, lon=31.48)
 print(coordinate._asdict())
+
+print(display(coordinate))
