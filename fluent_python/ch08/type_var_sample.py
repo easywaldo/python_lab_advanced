359ec70da3b023efdf69593b32c3131a0f2d4a9a
easywaldo <easywaldoji@gmail.com>
2023-01-30 23:57:37 +0900

    Spam class supports sorting

--- fluent_python/ch08/type_var_sample.py
+++ fluent_python/ch08/type_var_sample.py
@@ -44,4 +44,14 @@ print(top(l2, 3))
 
 l = [object() for _ in range(4)]
 print(l)
-print(sorted(l))    # TypeError: '<' not supported between instances of 'object' and 'object'
+# print(sorted(l))    # TypeError: '<' not supported between instances of 'object' and 'object'
+
+
+class Spam:
+    def __init__(self, n): self.n = n
+    def __lt__(self, other): return self.n < other.n
+    def __repr__(self) -> str: return f'Spam({self.n})'
+
+l = [Spam(n) for n in range(5, 0, -1)]
+print(l)
+print(sorted(l))        
