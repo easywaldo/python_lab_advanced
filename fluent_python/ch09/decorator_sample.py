79071d8f0ffcc6a3c7be90d0f22c8b9077404965
easywaldo <easywaldoji@gmail.com>
2023-02-01 23:01:51 +0900

    the registration module - decorator

--- fluent_python/ch09/decorator_sample.py
+++ fluent_python/ch09/decorator_sample.py
@@ -7,3 +7,35 @@ def target():
     print('running target()')
 
 target()
+
+print('\n================================================================\n')
+
+
+registry = []
+def register(func):
+    print(f'running register({func}')
+    registry.append(func)
+    return func
+
+@register
+def f1():
+    print('running f1()')
+    
+@register
+def f2():
+    print('running f2()')
+    
+def f3():
+    print('running f3()')
+    
+def main():
+    print('running main()')
+    print('registry main()')
+    f1()
+    f2()
+    f3()
+    print('regisry =================================================================')
+    print(registry)
+    
+if __name__ == '__main__':
+    main()
