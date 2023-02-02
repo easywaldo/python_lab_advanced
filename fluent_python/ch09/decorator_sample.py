f80db6468577512becbf28a87dcf6dc8a0363c94
easywaldo <easywaldoji@gmail.com>
2023-02-02 23:47:09 +0900

    a higher-order function to calculate a running average

--- fluent_python/ch09/decorator_sample.py
+++ fluent_python/ch09/decorator_sample.py
@@ -62,3 +62,21 @@ avg = Averager()
 print(avg(10))
 print(avg(11))
 print(avg(12))
+
+
+print('\n================================================================\n')
+
+def make_averager():
+    series = []
+    
+    def averager(new_value):
+        series.append(new_value)
+        total = sum(series)
+        return total / len(series)
+    
+    return averager
+
+avg = make_averager()
+print(avg(10))
+print(avg(11))
+print(avg(15))
