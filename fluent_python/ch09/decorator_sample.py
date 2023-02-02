7d423e5633761cd0f055652f6a1643c4cd80d014
easywaldo <easywaldoji@gmail.com>
2023-02-02 23:52:55 +0900

    __closure__ inforamtion

--- fluent_python/ch09/decorator_sample.py
+++ fluent_python/ch09/decorator_sample.py
@@ -80,3 +80,8 @@ avg = make_averager()
 print(avg(10))
 print(avg(11))
 print(avg(15))
+
+print(avg.__code__.co_varnames)
+print(avg.__code__.co_freevars)
+print(avg.__closure__)
+print(avg.__closure__[0].cell_contents)
