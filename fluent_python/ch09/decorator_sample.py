69e16e0163ea67efa1f641a4ba84596fedc59f53
easywaldo <easywaldoji@gmail.com>
2023-02-04 22:38:36 +0900

    faster implementation using caching

--- fluent_python/ch09/decorator_sample.py
+++ fluent_python/ch09/decorator_sample.py
@@ -187,3 +187,15 @@ def fibonacci(n):
 
 if __name__ == '__main__':
     print(fibonacci(6))
+    
+    
+@functools.cache
+@clock
+def fibonacci(n):
+    if n < 2:
+        return n
+    return fibonacci(n - 2) + fibonacci(n - 1)
+
+if __name__ == '__main__':
+    print(fibonacci(6))
+
