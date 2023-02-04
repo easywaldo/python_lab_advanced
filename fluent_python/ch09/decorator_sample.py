71c7c7249531507b5ca7bd6b68b189ef0af1e2a8
easywaldo <easywaldoji@gmail.com>
2023-02-04 22:58:08 +0900

    functools lru cache

--- fluent_python/ch09/decorator_sample.py
+++ fluent_python/ch09/decorator_sample.py
@@ -198,4 +198,16 @@ def fibonacci(n):
 
 if __name__ == '__main__':
     print(fibonacci(6))
+    
+
+from functools import lru_cache
+@lru_cache(maxsize=2**20, typed=True)
+@clock
+def fibonacci(n):
+    if n < 2:
+        return n
+    return fibonacci(n - 2) + fibonacci(n - 1)
+
+if __name__ == '__main__':
+    print(fibonacci(128))
 
