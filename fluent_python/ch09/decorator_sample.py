3777fca85ecd7c271c02b8c14442c1f6068bc6d7
easywaldo <easywaldoji@gmail.com>
2023-02-05 12:31:00 +0900

    class based clock decorator

--- fluent_python/ch09/decorator_sample.py
+++ fluent_python/ch09/decorator_sample.py
@@ -347,3 +347,31 @@ def snooze(seconds):
 
 for i in range(3):
     snooze(.123)
+    
+    
+print('================================================\n')
+
+class clock:
+    def __init__(self, fmt=DEFAULT_FMT):
+        self.fmt = fmt
+        
+    def __call__(self, func):
+        def clocked(*_args):
+            t0 = time.perf_counter()
+            _result = func(*_args)
+            elapsed = time.perf_counter() - t0
+            name = func.__name__
+            args = ', '.join(repr(arg) for arg in _args)
+            print(f'args {args}')
+            result = repr(_result)
+            print(self.fmt.format(**locals()))
+            return _result
+        return clocked
+
+
+@clock('{name}: {elapsed}s')
+def snooze(seconds):
+    time.sleep(seconds)
+
+for i in range(3):
+    snooze(.123)
