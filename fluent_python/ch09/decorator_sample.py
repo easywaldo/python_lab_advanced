268bfd15bd73982318e4dc0618693aecab6b01ea
easywaldo <easywaldoji@gmail.com>
2023-02-05 12:16:41 +0900

    other format paramerized clock decorator

--- fluent_python/ch09/decorator_sample.py
+++ fluent_python/ch09/decorator_sample.py
@@ -340,3 +340,10 @@ if __name__ == '__main__':
         
     for i in range(3):
         snooze(.123)
+
+@clock('{name}({args}) dt={elapsed:0.3f}s')
+def snooze(seconds):
+    time.sleep(seconds)
+
+for i in range(3):
+    snooze(.123)
