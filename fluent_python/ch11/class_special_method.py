002bd64e05d6c9915e3060340e7954e80ca77ab1
easywaldo <easywaldoji@gmail.com>
2023-02-19 23:01:41 +0900

    OpenPixel is a subclass of Pixel

--- fluent_python/ch11/class_special_method.py
+++ fluent_python/ch11/class_special_method.py
@@ -223,4 +223,18 @@ p = Pixel()
 
 p.x = 10
 p.y = 20
-p.color = 'red' # AttributeError: 'Pixel' object has no attribute 'color'
+# p.color = 'red' # AttributeError: 'Pixel' object has no attribute 'color'
+
+
+class OpenPixel(Pixel):
+    pass
+
+op = OpenPixel()
+print(op.__dict__)
+
+op.x = 8
+print(op.__dict__)
+print(op.x)
+op.color = 'green'
+
+print(op.__dict__)
