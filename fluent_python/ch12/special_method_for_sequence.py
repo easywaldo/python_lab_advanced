2cba5d9b4852000e7bc2863b1ab39234a8665a2d
easywaldo <easywaldoji@gmail.com>
2023-02-22 22:25:33 +0900

    checking out the behavior of __getitem__ and slices

---
+++ fluent_python/ch12/special_method_for_sequence.py
@@ -0,0 +1,14 @@
+class MySeq:
+    def __getitem__(self, index):
+        return index
+    
+
+
+if __name__ == '__main__':
+    s = MySeq()
+    print(s[1])
+    print(s[1:4])
+    print(s[1:4:2])
+    print(s[1:4:2, 9])
+    print(s[1:4:2, 7:9])
+    
