2bc49c52976c4ad0a58654de637aef157693416a
easywaldo <easywaldoji@gmail.com>
2023-02-22 23:01:46 +0900

    match context

--- expert_python/ch03/sample.py
+++ expert_python/ch03/sample.py
@@ -73,3 +73,15 @@ table_ref = {
 }
 sorter = TopologicalSorter(table_ref)
 print(list(sorter.static_order()))
+
+
+import sys
+match sys.platform:
+    case "windows":
+        print("Running on Windows")
+    case "darwin":
+        print("Running on macOS")
+    case "linux":
+        print("Running on Linux")
+    case _:
+        raise NotImplementedError(f"{sys.platform} not supported")
