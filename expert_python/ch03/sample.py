53a2ee07aec16ccdc0fcda59bd4e3c4c60cf2be3
easywaldo <easywaldoji@gmail.com>
2023-02-22 23:04:07 +0900

    match structual expression

--- expert_python/ch03/sample.py
+++ expert_python/ch03/sample.py
@@ -85,3 +85,10 @@ match sys.platform:
         print("Running on Linux")
     case _:
         raise NotImplementedError(f"{sys.platform} not supported")
+    
+for i in range(100):
+    match (i % 3, i % 5):
+        case (0, 0): print("FizzBuzz")
+        case (0, _): print("Fizz")
+        case (_, 0): print("Buzz")
+        case _: print(i)
