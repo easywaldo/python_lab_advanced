abe270f2ddb9fb85cab1afcc72c0aa9dd699623e
easywaldo <easywaldoji@gmail.com>
2023-03-02 08:37:39 +0900

    data class field attribute sample

--- data_class_sample.py
+++ data_class_sample.py
@@ -1,17 +1,21 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
 class C:
-    a: int
-    b: int  = 10
+    x: int
+    y: int = field(repr=False)
+    z: int = field(repr=True, default=10)
+    t: int = 20
     
     # init is primary default value
-    def __init__(self, a: int, b: int = 100):
-        self.a = a
-        self.b = b
+    def __init__(self, x: int = 10, y: int = 10, z: int = 10, t: int = 100):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.t = t
         
-c_data = C(a=1)
+c_data = C()
 print(c_data)
     
     
