2991826915f501a9402e31e4e7d0291ff5cf8d0c
easywaldo <easywaldoji@gmail.com>
2023-03-01 23:05:53 +0900

    Iterable class and Iterator class and Iterator Implementing class

--- fluent_python/ch10/sequence_sample.py
+++ fluent_python/ch10/sequence_sample.py
@@ -42,3 +42,39 @@ if __name__ == '__main__':
     print(list(iter(s3)))
     
     
+class Sentence:
+    def __init__(self, text):
+        self.text = text
+        self.words = RE_WORD.findall(text)
+        
+    def __repr__(self):
+        return 'Sentence(%s)' % reprlib.repr(self.text)
+    
+    def __iter__(self):
+        return SentenceIterator(self.words)
+    
+class SentenceIterator:
+    def __init__(self, words):
+        self.words = words
+        self.index = 0
+        
+    def __next__(self):
+        try:
+            word = self.words[self.index]
+        except IndexError:
+            raise StopIteration("no more next elements")
+        self.index += 1
+        return word
+    
+    def __iter__(self):
+        return self
+
+
+print('================================================================')
+s3 = Sentence('광진 초등학교 1학년')
+it = iter(s3)
+print(it)
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
