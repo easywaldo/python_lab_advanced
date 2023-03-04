4aafa1551dd47d65c68e7863a2c77ddeef962f6e
easywaldo <easywaldoji@gmail.com>
2023-03-04 13:05:57 +0900

    Collection sample

---
+++ collection_sample.py
@@ -0,0 +1,13 @@
+from collections import namedtuple
+
+
+Point = namedtuple('Point', ['x', 'y'])
+
+p = Point(11, y=22)
+print(p)
+print(p[0] + p[1])
+
+x, y = p
+print(x, y)
+print(p.x + p.y)
+print(p)
