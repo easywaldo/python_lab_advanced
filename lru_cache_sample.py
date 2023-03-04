7a3eb71c6371cf0eba84ba14df42ca03e9b8c81c
easywaldo <easywaldoji@gmail.com>
2023-03-04 12:40:18 +0900

    lru chached sample

---
+++ lru_cache_sample.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+
+def fetch_user(user_id):
+    print('loading db...')
+    return {
+        "userId": user_id,
+        "email": f"{user_id}@email.com",
+        "location": f"loc-{user_id}",
+    }
+    
+@lru_cache
+def get_user(user_id):
+    return fetch_user(user_id)
+
+from random import choice
+if __name__ == '__main__':
+    for i in range(10):
+        get_user(user_id=choice(["alpha", "beta", "bravo"]))
+    print(get_user.cache_info())
+    
