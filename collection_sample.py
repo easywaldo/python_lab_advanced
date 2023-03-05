efd03fc2822969239163e7d7dbd427ec1e5e7b75
easywaldo <easywaldoji@gmail.com>
2023-03-05 09:53:29 +0900

    deque sample

--- collection_sample.py
+++ collection_sample.py
@@ -11,3 +11,35 @@ x, y = p
 print(x, y)
 print(p.x + p.y)
 print(p)
+
+from collections import deque
+
+d = deque('ghi')
+for elem in d:
+    print(elem.upper())
+
+d.append('j')
+d.appendleft('f')
+print(d)
+
+d.pop()
+d.popleft()
+print(list(d))
+print(d[0])
+print(d[-1])
+
+print(list(reversed(d)))
+print('h' in d)
+d.extend('jkl')
+print(d)
+d.rotate(1)
+print(d)
+d.rotate(-1)
+print(d)
+
+print(reversed(d))
+d.clear()
+# d.pop() # IndexError: pop from an empty deque
+d.extendleft('abc')
+print(d)
+
