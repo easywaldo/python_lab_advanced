63bfc65c68ce85828ab681f78926d974b8863f03
easywaldo <easywaldoji@gmail.com>
2023-03-13 22:33:14 +0900

    zip function can also be used to transpose a matrix represnted as nested iterable.

--- zip_sample.py
+++ zip_sample.py
@@ -5,3 +5,11 @@ print(list(zip_sample))
 print(list(zip(range(3), 'ABC', [0.0, 1.1, 2.2, 3.3])))
 from itertools import zip_longest
 print(list(zip_longest(range(3), 'ABC', [0.0, 1.1, 2.2, 3.3], fillvalue=-1)))
+
+a = [(1,2,3), (4,5,6)]
+zip_list = list(zip(*a))
+print(zip_list)
+
+b = [(1,2), (3,4), (5,6)]
+zip_list = list(zip(*b))
+print(zip_list)
