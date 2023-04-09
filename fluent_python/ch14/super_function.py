e24cdb27cdd2bb6b493e4125760a3d58a6da695b
easywaldo <easywaldoji@gmail.com>
2023-04-09 23:17:10 +0900

    DoppelDict2 and AnswerDict2 works as expected because they extend UserDict and not dict

--- fluent_python/ch14/super_function.py
+++ fluent_python/ch14/super_function.py
@@ -44,3 +44,32 @@ print(d['a'])
 print(d)
 
 print(d['a'])   # ignored AnswerDict.__getitem__.
+
+
+print('================================')
+import collections
+
+class DoppelDict2(collections.UserDict):
+    def __setitem__(self, key, value):
+        super().__setitem__(key, [value] * 2)
+    
+dd = DoppelDict2(one=1)
+print(dd)
+dd['two'] = 2
+print(dd)
+
+dd.update(three=3)
+print(dd)
+
+class AnswerDict2(collections.UserDict):
+    def __getitem__(self, key):
+        return 42
+    
+ad = AnswerDict2(a='foo')
+print(ad['a'])
+
+d = {}
+d.update(ad)
+print(d['a'])
+print(d)
+
