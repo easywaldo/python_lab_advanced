02e595349e4ab341edbc18cdbc150246d60241ca
easywaldo <easywaldoji@gmail.com>
2023-05-01 23:02:42 +0900

    mypy reports violations of the parameterized type with helpful messages.

--- fluent_python/ch15/generic_lotto_demo.py
+++ fluent_python/ch15/generic_lotto_demo.py
@@ -57,3 +57,8 @@ class LottoBlower(Tombola, Generic[T]):
 machine = LottoBlower[int](range(1, 11))
 first = machine.pick()
 remain = machine.inspect()
+
+
+machine = LottoBlower[int]([1, .2])
+machine = LottoBlower[int](range(1, 11))
+machine.load('ABC')
