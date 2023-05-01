d8ee739aef8bacbadb9731e06f1a74907960bed5
easywaldo <easywaldoji@gmail.com>
2023-05-01 22:40:32 +0900

    from_json returns an invalid BookDict, and to_xml accepts it

--- fluent_python/ch15/books.py
+++ fluent_python/ch15/books.py
@@ -46,3 +46,22 @@ def from_json(data: str) -> BookDict:
 
 json_str = """{"isbn": "091234123", "title": "good books", "authors": ["john", "hopkins"], "pagecount": 10}"""
 print(from_json(json_str))
+
+
+NOT_BOOK_JSON = """
+        {"title": "Andromeda Strain",
+         "flavor": "pistachio",
+         "authors": true}
+    """
+    
+not_book = from_json(NOT_BOOK_JSON)
+if TYPE_CHECKING:
+    reveal_type(not_book) # evealed type is "TypedDict('books.BookDict', {'isbn': builtins.str, 'title': builtins.str, 'authors': builtins.list[builtins.str], 'pagecount': builtins.int})"
+    reveal_type(not_book['authors'])    # Revealed type is "builtins.list[builtins.str]"
+
+print(not_book)
+print(not_book['flavor'])   # Expression has type "Any"  , TypedDict "BookDict" has no key "flavor"
+
+xml = to_xml(not_book)
+print(xml)
+
