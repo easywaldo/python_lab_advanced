8c9965e94ea116c6d8f758ec92455657c63e5792
easywaldo <easywaldoji@gmail.com>
2023-05-06 23:05:07 +0900

    vector class operator @ methods and eq methods

--- fluent_python/ch16/vectory_multiply.py
+++ fluent_python/ch16/vectory_multiply.py
@@ -1,3 +1,4 @@
+from collections import abc
 from array import array
 import reprlib
 
@@ -26,6 +27,28 @@ class Vector:
     def __rmul__(self, scalar):
         return self * scalar
     
+    def __matmul__(self, other):
+        if (isinstance(other, abc.Sized) and
+            isinstance(other, abc.Iterable)):
+            if len(self) == len(other):
+                return sum(a * b for a, b in zip(self, other))
+            else:
+                raise ValueError('@ requires vectors of equal length.')
+        else:
+            return NotImplemented
+    
+    def __rmatmul__(self, other):
+        return self @ other
+    
+    def __len__(self):
+        return len(self._components)
+    
+    def __eq__(self, other):
+        return (len(self) == len(other) and
+                all(a == b for a, b in zip(self, other)))
+    
+    
+    
 
 v1 = Vector([1.0, 2.0, 3.0])
 print(14 * v1)
@@ -34,3 +57,12 @@ print(v1 * True)
 
 from fractions import Fraction
 print(v1 * Fraction(1, 3))
+
+
+print('================================================================')
+print(Vector([10, 20, 30]) == Vector([10, 20, 30]))
+
+print('================================================================')
+va = Vector([1,2,3])
+vz = Vector([5,6,7])
+print(va @ vz == 38.0)
