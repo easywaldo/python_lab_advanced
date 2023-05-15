7bbce7ccedb16bc1bd1085ae48d756ac06cf1cea
jinam.lee <jinam.lee@a-bly.com>
2023-05-15 11:37:24 +0900

    TypedDict sample

--- fluent_python/ch15/books.py
+++ fluent_python/ch15/books.py
@@ -1,4 +1,4 @@
-from typing import TypedDict
+from typing import TYPE_CHECKING, TypedDict
 
 
 class BookDict(TypedDict):
@@ -21,3 +21,23 @@ print(type(pp))
 # print(pp.title) # AttributeError: 'dict' object has no attribute 'title'
 print(pp['title'])
 print(pp.get('title'))
+
+
+def demo() -> None:
+    book = BookDict(
+        isbn='0134757599',
+        title='Refactoring, 2e',
+        authors=['Martin Fowler', 'Kent Beck'],
+        pagecount=478
+    )
+    authors = book['authors']
+    if TYPE_CHECKING:
+        print('====')
+        reveal_type(authors)
+    authors = 'Bob'
+    book['weight'] = 4.2
+    del book['title']
+    print(book)
+
+if __name__ == '__main__':
+    demo()
