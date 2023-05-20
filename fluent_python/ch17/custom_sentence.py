9adc262b0bc59e611462e7846dd4616abcac4d62
easywaldo <easywaldoji@gmail.com>
2023-05-20 12:05:35 +0900

    classroom and student class iteration

--- fluent_python/ch17/custom_sentence.py
+++ fluent_python/ch17/custom_sentence.py
@@ -20,3 +20,28 @@ class Sentence:
 language_list = Sentence('Python Kotlin C# Java PHP Javascript SQL Go Typescript C++ VisualBasic')
 for lang in language_list:
     print(lang)
+    
+    
+numbers = [1,2,3,4,5,6,7,8,9,10]
+n_iter = iter(numbers)
+print(next(n_iter))
+print(next(n_iter))
+print(next(n_iter))
+print(next(n_iter))
+print(next(n_iter))
+
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+class Classroom:
+    def __init__(self, students):
+        self.students = students
+        
+    def __iter__(self):
+        for student in self.students:
+            yield student
+            
+class_room = Classroom([Student(name="easywaldo"), Student(name="romeo")])
+for student in class_room:
+    print(student.name)
