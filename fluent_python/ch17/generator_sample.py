786544a7d7840393526d7a932bb6ce62f6c4db31
easywaldo <easywaldoji@gmail.com>
2023-05-26 23:39:46 +0900

    itertools other functions

--- fluent_python/ch17/generator_sample.py
+++ fluent_python/ch17/generator_sample.py
@@ -59,3 +59,11 @@ def vowel(c):
     return c.lower() in 'aeiou'
 
 print(list(filter( vowel, 'Aardvark')))
+
+print(list(itertools.filterfalse(vowel, 'Aardvark')))
+print(list(itertools.dropwhile(vowel, 'Aardvark')))
+print(list(itertools.takewhile(vowel, 'Aardvark')))
+print(list(itertools.compress('Aardvark', (1, 0, 1, 1, 0, 1))))
+print(list(itertools.islice('Aardvardk', 4)))
+print(list(itertools.islice('Aardvark', 4, 7)))
+print(list(itertools.islice('Aardvark', 1, 7, 2)))
