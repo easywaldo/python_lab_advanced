269c43fafe343b1e81a11fbfef304660706df797
easywaldo <easywaldoji@gmail.com>
2023-05-31 22:24:05 +0900

    built-in any function

--- fluent_python/ch17/generator_sample.py
+++ fluent_python/ch17/generator_sample.py
@@ -90,3 +90,15 @@ print(list(itertools.starmap(lambda a, b: b / a, enumerate(itertools.accumulate(
 
 print(all([1, 2, 3]))
 print(all([1, 0, 3]))
+
+
+print(all([]))
+print(any([1, 2, 3]))
+print(any([1, 0, 3]))
+print(any([0, 0.0]))
+print(any([]))
+
+g = (n for n in [0, 0.0, 7, 8])
+print(any(g))
+print(next(g))
+
