72be7058f2ce04a9ae52f9e3223bd3a2256d4421
easywaldo <easywaldoji@gmail.com>
2023-06-10 22:33:05 +0900

    Excercising LookingGlass without a with block

--- fluent_python/ch18/looking_glass.py
+++ fluent_python/ch18/looking_glass.py
@@ -7,3 +7,19 @@ with LookingGlass() as what:
 print(what)
 
 print('Back to normal.')
+
+
+
+print('================================================================')
+manager = LookingGlass()
+print(manager)
+
+monster = manager.__enter__()
+print(monster == 'JABBERWOCKY')
+
+print(monster)
+
+print(manager)
+
+manager.__exit__(None, None, None)
+print(monster)
