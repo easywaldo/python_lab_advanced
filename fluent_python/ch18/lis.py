7759f39f995bd0a075ef1eadb618f30849c9c6c3
easywaldo <easywaldoji@gmail.com>
2023-06-11 22:02:13 +0900

    Enviromment class derived from ChaingMap

--- fluent_python/ch18/lis.py
+++ fluent_python/ch18/lis.py
@@ -8,3 +8,26 @@ Symbol: TypeAlias = str
 Atom: TypeAlias = float | int | Symbol
 Expression: TypeAlias = Atom | list
 
+
+class Environment(ChainMap[Symbol, Any]):
+    def change(self, key: Symbol, value: Any) -> None:
+        for map in self.maps:
+            if key in map:
+                map[key] = value
+                return
+        raise KeyError(key)
+
+inner_env = {'a': 2}
+outer_env = {'a': 0, 'b': 1}
+env = Environment(inner_env, outer_env)
+print(env['a'])
+print(env)
+
+env['a'] = 111
+env['c'] = 222
+
+print(env)
+
+env.change('b', 333)
+print(env)
+
