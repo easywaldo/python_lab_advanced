7a98d777d10de82eef84f87d0780c8fca8932f05
easywaldo <easywaldoji@gmail.com>
2023-06-11 21:54:59 +0900

    use of TypeAlias and the | type union operator require Python 3.10

---
+++ fluent_python/ch18/lis.py
@@ -0,0 +1,10 @@
+import math
+import operator as op
+from collections import ChainMap
+from itertools import chain
+from typing import Any, TypeAlias, NoReturn
+
+Symbol: TypeAlias = str
+Atom: TypeAlias = float | int | Symbol
+Expression: TypeAlias = Atom | list
+
